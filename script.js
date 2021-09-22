7e8ab463af9b2756ae8da121f165d1015def6d99
Michael Desanker <mdesanker@gmail.com>
2021-09-22 16:25:35 -0400

    Basic operator functions created

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // script.js //
+
+// Functions for basic math operators
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
