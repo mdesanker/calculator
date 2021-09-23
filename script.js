75116a94793a27a61aed31e38b0685193689640d
Michael Desanker <mdesanker@gmail.com>
2021-09-23 08:07:05 -0400

    Separate function for clear button which clears stored values versus the display clear that occurs when an operator button is pressed

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ function divide(a, b) {
     return a / b;
 }
 
-// function operate(a, b, operator) {
-//     return operator(a, b);
-// }
-
 // Operate function using switch statement
 function operate(a, b, operator) {
     switch (operator) {
@@ -31,9 +27,15 @@ function operate(a, b, operator) {
     }
 }
 
-function clear() {
+function clearData() {
+    entry1 = '';
+    entry2 = '';
+    operation = '';
     display.textContent = '';
-    // equationDisplay.textContent = '';
+}
+
+function clearDisplay() {
+    display.textContent = '';
 }
 
 function addToDisplay(val) {
@@ -42,12 +44,16 @@ function addToDisplay(val) {
     display.textContent = update;
 }
 
+let entry1
+let operation
+let entry2
+
 const display = document.querySelector('#display');
 const equationDisplay = document.querySelector('#calculation')
 
 // Clear button functionality
 const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clear);
+clearButton.addEventListener('click', clearData);
 
 // Check for number button press and get textContent of respective button
 const numButtons = document.querySelectorAll('button.num');
@@ -58,19 +64,15 @@ numButtons.forEach(button => {
     })
 });
 
-let entry1
-let operation
-let entry2
-
 // Check for operator button press and get id of respective button
 const operButtons = document.querySelectorAll('button.operator');
 operButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if (display.textContent) {
+        if (display.textContent) {  // If already number in display (from previous calc), use that as the first entry in next calculation
             entry1 = parseInt(display.textContent);
             operation = button.id;
             console.log(entry1, operation);
-            clear();
+            clearDisplay();
         }
     })
 })
